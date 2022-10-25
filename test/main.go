916b96891cdb1148b7d0de57052c65098c152cd5
Abduino <abdrehmanredi0@gmail.com>
2022-10-25 15:50:53 -0300

    go variables and constants

--- test/main.go
+++ test/main.go
@@ -3,5 +3,23 @@ package main
 import "fmt"
 
 func main(){
+	// go lang output statement 
 	fmt.Print("hello world")
+	fmt.Println("this is new line")
+	// go variables 
+
+	var age = 90
+	var name = "ABDURHMAN"
+
+	fmt.Println("my name is ", name , "and i am ", age , "years old")
+
+
+	var fName string
+	var price int 
+	fName = "Redi"
+	price = 50
+
+	fmt.Println("my father name is ", fName)
+	fmt.Println("monthly price is", price)
+
 }
